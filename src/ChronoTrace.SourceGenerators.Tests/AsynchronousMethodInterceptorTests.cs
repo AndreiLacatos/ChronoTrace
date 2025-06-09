c977c3e6b0da5cefaecd680fe44ddd00a7c8e343
AndreiLacatos <lacatos_andrei@yahoo.com>
2025-06-09 18:55:22 +0300

    #12 Added tests for methods with simple parameters

--- src/ChronoTrace.SourceGenerators.Tests/AsynchronousMethodInterceptorTests.cs
+++ src/ChronoTrace.SourceGenerators.Tests/AsynchronousMethodInterceptorTests.cs
@@ -95,4 +95,27 @@ public class AsynchronousMethodInterceptorTests
         var driver = SourceGenerationRunner.Run(source);
         await Verify(driver).UseDirectory(TestConstants.SnapshotsDirectory);
     }
+
+    [Fact]
+    public async Task AsyncMethodWithParametersSingularInvocation_ShouldGenerateSingularAsyncInterceptor()
+    {
+        var source = 
+            """
+            using System.Threading.Tasks;
+
+            public class S
+            {
+                [ChronoTrace.Attributes.Profile]
+                public async Task Do(int p1, string p2, double p3)
+                {
+                }
+            }
+
+            var subject = new S();
+            await subject.Do(default, default, default);
+            """;
+
+        var driver = SourceGenerationRunner.Run(source);
+        await Verify(driver).UseDirectory(TestConstants.SnapshotsDirectory);
+    }
 }
--- src/ChronoTrace.SourceGenerators.Tests/SynchronousMethodInterceptorTests.cs
+++ src/ChronoTrace.SourceGenerators.Tests/SynchronousMethodInterceptorTests.cs
@@ -44,4 +44,25 @@ public class SynchronousMethodInterceptorTests
         var driver = SourceGenerationRunner.Run(source);
         await Verify(driver).UseDirectory(TestConstants.SnapshotsDirectory);
     }
+
+    [Fact]
+    public async Task SyncMethodWithParametersSingularInvocation_ShouldGenerateSingularSyncInterceptor()
+    {
+        var source = 
+            """
+            public class S
+            {
+                [ChronoTrace.Attributes.Profile]
+                public void Do(int p1, string p2, double p3)
+                {
+                }
+            }
+
+            var subject = new S();
+            subject.Do(default, default, default);
+            """;
+
+        var driver = SourceGenerationRunner.Run(source);
+        await Verify(driver).UseDirectory(TestConstants.SnapshotsDirectory);
+    }
 }
