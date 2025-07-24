16a645874a4c84da5ae1cd78a9f6f6b592a00fab
AndreiLacatos <lacatos_andrei@yahoo.com>
2025-07-24 22:36:03 +0300

    #19 Switch to netstandard2.1 on ChronoTrace.Attributes

--- src/ChronoTrace.Attributes/ProfileAttribute.cs
+++ src/ChronoTrace.Attributes/ProfileAttribute.cs
@@ -1,7 +1,10 @@
-namespace ChronoTrace.Attributes;
+using System;
 
-/// <summary>
-/// Marks a method to be included in performance tracing by the <c>ChronoTrace</c> library.
-/// </summary>
-[AttributeUsage(AttributeTargets.Method, Inherited = false)]
-public sealed class ProfileAttribute : Attribute;
+namespace ChronoTrace.Attributes
+{
+    /// <summary>
+    /// Marks a method to be included in performance tracing by the <c>ChronoTrace</c> library.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    public sealed class ProfileAttribute : Attribute { }
+}
